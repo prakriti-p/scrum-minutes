792172dcc67b2bfe20e3a264246f5ca135eecad1
Prakriti <prakriti.patra@gmail.com>
2018-08-15 17:58:49 +0530

    Adding redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+/* import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
+import index from "./js/index"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root")); */
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import "./index.css";
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
+import index from "./js/index"
+
+ReactDOM.render(<Provider store={store}>
+                <App />
+                </Provider>, 
+            document.getElementById("root"));
+
